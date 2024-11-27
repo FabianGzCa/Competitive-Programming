bad601a90eb5399c775ff9afc1dc93b1c164f9d4
Fabián González Castañeda <fabiangzca@gmail.com>
2024-11-26 18:44:38 -0600

    Parallel Projection
    Solved

---
+++ Codeforces/Problemset/800/Parallel Projection/Parallel Projection.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	int inputs=0;
+	cin>>inputs;
+	while (inputs--){
+		int w, d, h;
+		int a, b, f, g;
+		int ans;
+		cin >> w >> d >> h;
+		cin >> a >> b >> f >> g;
+
+		ans = b + abs(a-f) + g;
+		ans = min(ans, d-b + abs(a-f) + d-g);
+		ans = min(ans, a + abs(b-g) + f);
+		ans = min(ans, w-a + abs(b-g) + w-f);
+		ans += h;
+		cout<<ans<<"\n";
+	}
+
+	return 0;
+}

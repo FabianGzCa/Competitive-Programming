5a790b49902b4522801a495493d93083dd9e0916
Fabián González Castañeda <fabiangzca@gmail.com>
2025-04-07 15:26:22 -0600

    Plantilla movida

--- Codeforces/Problemset/Plantilla.cpp
+++ Codeforces/Problemset/Plantilla.cpp
@@ -1,11 +1,16 @@
-// problem: 
+//Problem: 
 
 #include<iostream>
 using namespace std;
 
+void solve(){
+	int a;
+}
+
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
-	
-	
+	int tt;
+	cin>>tt;
+	while(tt--) solve();
 	return 0;
 }

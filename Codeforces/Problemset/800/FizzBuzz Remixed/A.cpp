d4cdeebd5ce88653785c32b29dce17318ee77bae
Fabián González Castañeda <fabiangzca@gmail.com>
2025-04-10 15:35:14 -0600

    FizzBuzz Remixed solved

---
+++ Codeforces/Problemset/800/FizzBuzz Remixed/A.cpp
@@ -0,0 +1,26 @@
+//Problem: https://codeforces.com/contest/2070/problem/A
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+void solve(){
+	int num, res, extra=0;
+	cin>>num;
+	res = num/15;
+	if (res*15+1==num) extra++;
+	else if (res*15+2 <= num) extra+=2;
+	res *=3;
+	res +=1;
+	res +=extra;
+	cout<<res<<endl;
+}
+
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	int tt;
+	cin>>tt;
+	while(tt--) solve();
+	return 0;
+}

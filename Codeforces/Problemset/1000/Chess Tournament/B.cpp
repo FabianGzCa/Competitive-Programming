dafc4e1935237d85d8a81477531f21c763f1f314
Fabián González Castañeda <fabiangzca@gmail.com>
2025-04-27 21:48:32 -0600

    Chess Tournament Wrong answer
    Identificaba derrotas fantasma (caso de prueba: 4 2222)

---
+++ Codeforces/Problemset/1000/Chess Tournament/B.cpp
@@ -0,0 +1,77 @@
+//Problem: https://codeforces.com/contest/1569/problem/B
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int n, dos=0;
+	cin>>n;
+	vector<char> desiciones(n);
+	int primerGanador=-1, cantGanadores=0;
+
+	for(int i=0; i<n; i++){
+		cin>>desiciones[i];
+		if(desiciones[i]=='2'){
+			dos++;
+			if(primerGanador==-1){
+				primerGanador=i;
+			}
+			cantGanadores++;
+		}
+	}
+
+	if(dos==1 || dos==2){
+		cout<<"NO\n";
+		return;
+	}
+	cout<<"YES\n";
+	int cg=0;
+
+	//2122
+
+
+	for(int i=0; i<n; i++){
+		int cantidadDoses=0;
+		for(int j=0; j<n; j++){
+			if(i==j){
+				if(desiciones[i]=='2'){
+					cantidadDoses++;
+				}
+				cout<<"X";
+			}else if(desiciones[i]=='2'){
+				if(desiciones[j]=='1'){
+					cout<<"=";
+					cantidadDoses--;
+				}else if(cg<cantGanadores-1 && cantidadDoses==cg+1){
+					cout<<"+";
+				}else if(cg<cantGanadores-1){
+					cout<<"-";
+				}else if(cg==cantGanadores-1){
+					if(j==primerGanador){
+						cout<<"+";
+					}else{
+						cout<<"-";
+					}
+				}
+				cantidadDoses++;
+			}else{
+				cout<<"=";
+			}
+		}
+		if(desiciones[i] == '2'){
+			cg++;
+		}
+		cout<<"\n";
+	}
+}
+
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	int tt;
+	cin>>tt;
+	while(tt--) solve();
+	return 0;
+}

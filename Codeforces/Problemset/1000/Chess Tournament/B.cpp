e6e312b124b88389e77a0ca5fc75a5d3037780db
Fabián González Castañeda <fabiangzca@gmail.com>
2025-04-28 13:33:51 -0600

    Chess Tournament solved

--- Codeforces/Problemset/1000/Chess Tournament/B.cpp
+++ Codeforces/Problemset/1000/Chess Tournament/B.cpp
@@ -7,79 +7,95 @@
 using namespace std;
 
 void solve(){
-	int n, dos=0;
+	// Input
+	int n, totalTwo=0;
 	cin>>n;
-	vector<char> desiciones(n);
-	int primerGanador=-1, cantGanadores=0;
-
+	vector<char> partidas;
+	char cu;
 	for(int i=0; i<n; i++){
-		cin>>desiciones[i];
-		if(desiciones[i]=='2'){
-			dos++;
-			if(primerGanador==-1){
-				primerGanador=i;
-			}
-			cantGanadores++;
+		cin>>cu;
+		partidas.push_back(cu);
+
+		if(cu=='2'){ //
+			totalTwo++;
 		}
 	}
 
-	if(dos==1 || dos==2){
+	// Solution when 1/2 select 2
+	if(totalTwo==1 || totalTwo==2){
 		cout<<"NO\n";
 		return;
+	}else{//else, is possible
+		cout<<"YES\n";
 	}
-	cout<<"YES\n";
-	int cg=0;
 
-	//2122
+	//output if it's possible
 
+	int numeroDos=0;
 
 	for(int i=0; i<n; i++){
-		int cantidadDoses=0, cantidadNegativos=cantGanadores, nohubo=1;
-		for(int j=0; j<n; j++){
-			if(i==j){
-				if(desiciones[i]=='2'){
-					cantidadDoses++;
-				}
-				cout<<"X";
-			}else if(desiciones[i]=='2'){
-				if(desiciones[j]=='1'){
-					cout<<"=";
-					cantidadDoses--;
-				}else if(cg<cantGanadores-1 && cantidadDoses==cg+1){
-					cout<<"+";
-				}else if(cg<cantGanadores-1){
-					if((j+1 ==i || j+1 == n) && nohubo){
-						nohubo=0;
-						cout<<"-";
-					}else if(desiciones[j]=='1' || !nohubo){
-						cout<<"=";
-					}else{
-						nohubo=0;
-						cout<<"-";
-					}
-				}else if(cg==cantGanadores-1){
-					if(j==primerGanador){
-						cout<<"+";
-					}else if((j+1 ==i || j+1 == n) && nohubo){
-						cout<<"-";
-						nohubo=0;
-					}else if(desiciones[j]=='1' || !nohubo){
-						cout<<"=";
-					}else{
-						nohubo=0;
-						cout<<"-";
-					}
-				}
-				cantidadDoses++;
-			}else{
-				cout<<"=";
+
+		int cantDosPorFila=0;
+		if(partidas[i] == '2'){
+			numeroDos++;
+		}
+
+		for(int j=0; j<n; j++){//print the matrix
+
+			if(partidas[j] == '2'){
+				cantDosPorFila++;
 			}
+
+			if(i==j){ // X case
+				cout<<"X";
+				continue;
+			}
+
+			if(partidas[i]=='1'){//If the player selects 1, all matches result in a draw.
+				cout<<"=";
+				continue;
+			}
+
+			if(partidas[j]=='1'){//If playing against someone who selected 1, the match results in a draw.
+				cout<<"=";
+				continue;
+			}
+
+			
+			//+ case
+			if(numeroDos!=totalTwo){//No es el ultimo 2 (ultima fila)
+				if(cantDosPorFila == numeroDos+1){ //El siguiente 2 en columna al 2 de la fila actual.
+					cout<<"+";
+					continue;
+				}
+			}else{//Es el ultimo 2 (ultima fila)
+				if(cantDosPorFila==1){//Para el primer 2 que encuentra
+					cout<<"+";
+					continue;
+				}
+			}
+
+			// - & =
+			if(numeroDos!=1){//No es el primer 2 (primera fila)
+				if( cantDosPorFila == numeroDos-1){
+					cout<<"-";
+				}else{
+					cout<<"=";
+				}
+			}else{//Es el primer 2 (primera fila)
+				if(cantDosPorFila == totalTwo){//Si es el ultimo 2 de la fila
+					cout<<"-";
+				}else{
+					cout<<"=";
+				}
+			}
+
+			
 		}
-		if(desiciones[i] == '2'){
-			cg++;
-		}
-		cout<<"\n";
+		cout<<"\n"; //Terminamos una fila
 	}
+
+
 }
 
 int main(){

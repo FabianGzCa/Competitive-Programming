6c1e9943dd65e7b5fa041d5d2ce2832475b9452e
Fabián González Castañeda <fabiangzca@gmail.com>
2025-07-23 09:45:05 -0600

    A BIT of a Construction solved

---
+++ Codeforces/Problemset/1100/A BIT of a Construction/B.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+void solve(){
+    int ammount, sum;
+    std::cin>>ammount>>sum;
+    if(ammount==1){
+        std::cout<<sum<<"\n";
+        return;
+    }
+    int max=1;
+    while(sum>max){
+        max*=2;
+    }
+
+    if(sum == max-1 || sum==1){
+        std::cout<<sum<<" ";
+        for(int i=0; i<ammount-1; i++){
+            std::cout<<"0 ";
+        }
+        std::cout<<"\n";
+        return;
+    }
+
+    max/=2;
+    std::cout<<max-1<<" "<<sum-(max-1)<<" ";
+    for(int i=0; i<ammount-2; i++){
+        std::cout<<"0 ";
+    }
+    std::cout<<"\n";
+}
+
+int main(){
+    std::cin.tie(0)->sync_with_stdio(0);
+    int tt; std::cin>>tt;
+    while(tt--)solve();
+}

5e1897ade209369d7e0e27d09e56e445e18ac832
Fabián González Castañeda <fabiangzca@gmail.com>
2025-07-30 19:17:02 -0600

    A+B solved

---
+++ omegaUp/Cursos/Introductorio a la OMI/Programación básica/A.cpp
@@ -0,0 +1,9 @@
+//Problem: https://omegaup.com/course/Curso-OMI/assignment/COMI-programacion#problems/COMI-a-b
+
+#include<iostream>
+
+int main(){
+    int num1,num2;
+    std::cin>>num1>>num2;
+    std::cout<<num1+num2<<"\n";
+}
